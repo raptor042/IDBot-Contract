944dfb42a9dc3754d008c8618ac1dd0a66754e55
Benji Chimex <benjichimex@Benjis-MacBook-Pro.local>
2023-12-10 00:43:52 +0100

    another commit

--- contracts/IDBot.sol
+++ contracts/IDBot.sol
@@ -4,38 +4,135 @@ pragma solidity ^0.8.19;
 import "./Profile.sol";
 
 contract IDBot {
+    address owner;
+
+    address[] public _profiles;
+
     mapping (string => address) public profiles;
+
+    struct Subscription {
+        address account;
+        uint256 duration;
+        uint256 startedAt;
+        bool isActive;
+    }
+
+    Subscription[] public _subscribers;
+
+    mapping (address => Subscription) public subscribers;
 
     event CreateProfile(address indexed profile, address indexed owner, string profileId);
 
-    constructor() {}
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Not Authorized.");
+        _;
+    }
 
     function createProfile(
         string memory _name,
+        string memory _description,
+        bool _dev,
         string memory _email,
         uint256 _age,
         string memory phone,
+        string memory _country,
+        string memory _state,
+        string memory _address,
         string memory url,
-        address _owner
-    ) external {
+        address _owner,
+        string memory _hash
+    ) public onlyOwner {
         Profile profile = new Profile(
             _name,
+            _description,
+            _dev,
             _email,
-           _age,
+            _age,
             phone,
+            _country,
+            _state,
+            _address,
             url,
             _owner
         );
 
         string memory profileId = string(abi.encodePacked(
-            "did-IDBot",
-            address(profile),
+            "did-IDBot-",
+            _hash,
             "#",
             block.timestamp
         ));
 
+        _profiles.push(address(profile));
+
         profiles[profileId] = address(profile);
 
         emit CreateProfile(address(profile), _owner, profileId);
+    }
+
+    function subscribe(address _account, uint256 _duration) public onlyOwner {
+        if(isSubscribed(_account)) {
+            updateSubscription(_account, _duration);
+        } else {
+            Subscription memory subscription = Subscription({
+                account : _account,
+                duration : _duration,
+                startedAt : block.timestamp,
+                isActive : true
+            });
+
+            for (uint i = 0; i < _profiles.length; i++) {
+                Profile profile = Profile(_profiles[i]);
+
+                profile.addAccountToAccessList(_account);
+            }
+
+            _subscribers.push(subscription);
+
+            subscribers[_account] = subscription;
+        }
+    }
+
+    function unsubscribe() public onlyOwner {
+        for (uint i = 0; i < _subscribers.length; i++) {
+            Subscription storage subscriber = _subscribers[i];
+            uint256 timeElapsed = (block.timestamp - subscriber.startedAt) / 86400;
+
+            if(timeElapsed >= subscriber.duration && subscriber.isActive == true) {
+                subscriber.isActive = false;
+
+                for (uint x = 0; x < _profiles.length; x++) {
+                    Profile profile = Profile(_profiles[x]);
+
+                    profile.removeAccountFromAccessList(subscriber.account);
+                }
+            }
+        }
+    }
+
+    function isSubscribed(address _account) internal view onlyOwner returns (bool subscribed) {
+        Subscription memory subscriber = subscribers[_account];
+
+        if(_account == subscriber.account) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function updateSubscription(address _account, uint256 _duration) internal onlyOwner {
+        Subscription storage subscriber = subscribers[_account];
+
+        if(subscriber.isActive == false) {
+            subscriber.isActive = true;
+        }
+
+        subscriber.duration = _duration;
+
+        subscriber.startedAt = block.timestamp;
     }
 }
--- contracts/Profile.sol
+++ contracts/Profile.sol
@@ -2,39 +2,80 @@
 pragma solidity ^0.8.19;
 
 contract Profile {
-    address owner;
+    address private owner;
 
-    bool public verified;
+    address super_owner;
 
-    string public name;
+    bool private verified;
 
-    string public email;
+    string private name;
 
-    uint256 public age;
+    string private description;
 
-    string public phone_number;
+    bool private dev;
 
-    string public profile_pic_url;
+    string private email;
 
-    uint256 public reputation_score;
+    uint256 private age;
+
+    string private phone_number;
+
+    string private country;
+
+    string private state;
+
+    string private residential_address;
+
+    string private profile_pic_url;
+
+    uint256 private reputation_score;
 
     struct Project {
         string name;
+        string description;
+        address contract_address;
+        string website_link;
+        string telegram_link;
+        string twitter_link;
+        string discord_link;
+        string linktree;
+        bool isHoneyPot;
+        bool isRugged;
+        uint256 reputation_score;
     }
 
-    Project[] public _projects;
+    Project[] private _projects;
 
-    mapping (string => Project) public projects;
+    mapping (string => Project) private projects;
+
+    string[] private projectIds;
+
+    address[] private access_list;
+
+    mapping (address => bool) private accesslist;
+
+    event AddProject(address indexed ca, string name, string projectId);
 
     constructor(
         string memory _name,
+        string memory _description,
+        bool _dev,
         string memory _email,
         uint256 _age,
         string memory phone,
+        string memory _country,
+        string memory _state,
+        string memory _address,
         string memory url,
         address _owner
     ) {
+        verified = true;
+
         name = _name;
+
+        description = _description;
+
+        dev = _dev;
 
         email = _email;
 
@@ -42,15 +83,270 @@ contract Profile {
 
         phone_number = phone;
 
+        country = _country;
+
+        state = _state;
+
+        residential_address = _address;
+
         profile_pic_url = url;
 
         reputation_score = 0;
 
         owner = _owner;
+
+        super_owner = msg.sender;
+
+        access_list.push(owner);
+
+        access_list.push(super_owner);
+
+        accesslist[owner] = true;
+
+        accesslist[super_owner] = true;
     }
 
     modifier onlyOwner {
-        require(msg.sender == owner);
+        require(msg.sender == owner || msg.sender == super_owner, "Not Authorized.");
         _;
+    }
+
+    modifier onlySuperOwner {
+        require(msg.sender == super_owner, "Not Authorized.");
+        _;
+    }
+
+    function getOwner() public view returns (address) {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+
+        return owner;
+    }
+
+    function getName() public view returns (string memory) {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+
+        return name;
+    }
+
+    function getDescription() public view returns (string memory) {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+
+        return description;
+    }
+
+    function isDev() public view returns (bool) {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+
+        return dev;
+    }
+
+    function getEmail() public view returns (string memory) {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+
+        return email;
+    }
+
+    function getAge() public view returns (uint256) {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+
+        return age;
+    }
+
+    function getPhoneNumber() public view returns (string memory) {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+
+        return phone_number;
+    }
+
+    function getCountry() public view returns (string memory) {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+
+        return country;
+    }
+
+    function getState() public view returns (string memory) {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+
+        return state;
+    }
+
+    function getResidentialAddress() public view returns (string memory) {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+
+        return residential_address;
+    }
+
+    function getProfilePicUrl() public view returns (string memory) {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+
+        return profile_pic_url;
+    }
+
+    function getReputationScore() public view returns (uint256) {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+
+        return reputation_score;
+    }
+
+    function getProjectIds() public view returns (string[] memory) {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+
+        return projectIds;
+    }
+
+    function getAccessList() public view returns (address[] memory) {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+
+        return access_list;
+    }
+
+    function getAccountAccess(address account) public view returns (bool) {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+
+        return accesslist[account];
+    }
+
+    function getProjects() public view returns (Project[] memory) {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+
+        return _projects;
+    }
+
+    function addProject(
+        string memory _name,
+        string memory _description,
+        address _contract_address,
+        string memory _website_link,
+        string memory _telegram_link,
+        string memory _twitter_link,
+        string memory _discord_link,
+        string memory _linktree
+    ) public onlyOwner {
+        require(verified, "Not verified.");
+        Project memory project = Project({
+            name : _name,
+            description : _description,
+            contract_address : _contract_address,
+            website_link : _website_link,
+            telegram_link : _telegram_link,
+            twitter_link : _twitter_link,
+            discord_link : _discord_link,
+            linktree : _linktree,
+            isHoneyPot : false,
+            isRugged : false,
+            reputation_score : 100
+        });
+
+        string memory projectId = string(abi.encodePacked("IDBot-", owner, "#", block.timestamp));
+
+        _projects.push(project);
+
+        projects[projectId] = project;
+        
+        reputation_score += 100;
+
+        emit AddProject(_contract_address, _name, projectId);
+    }
+
+    function getProjectName(string memory id) public view returns (string memory) {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+        Project memory project = projects[id];
+
+        return project.name;
+    }
+
+    function getProjectDescription(string memory id) public view returns (string memory) {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+        Project memory project = projects[id];
+
+        return project.description;
+    }
+
+    function getProjectContractAddress(string memory id) public view returns (address) {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+        Project memory project = projects[id];
+
+        return project.contract_address;
+    }
+
+    function getProjectLinks(string memory id) public view returns (string[5] memory) {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+        Project memory project = projects[id];
+
+        string[5] memory links = [
+            project.website_link,
+            project.telegram_link,
+            project.twitter_link,
+            project.discord_link,
+            project.linktree
+        ];
+
+        return links;
+    }
+
+    function checkIfProjectIsHoneyPot(string memory id) public view returns (bool) {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+        Project memory project = projects[id];
+
+        return project.isHoneyPot;
+    }
+
+    function checkIfProjectIsRugged(string memory id) public view returns (bool) {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+        Project memory project = projects[id];
+
+        return project.isRugged;
+    }
+
+    function getProjectReputationScore(string memory id) public view returns (uint256) {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+        Project memory project = projects[id];
+
+        return project.reputation_score;
+    }
+
+    function reportProjectIsHoneyPot(string memory id) public {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+        Project storage project = projects[id];
+
+        if(project.isHoneyPot == false) {
+            project.isHoneyPot = true;
+        }
+
+        project.reputation_score -= 1;
+
+        reputation_score -= 1;
+    }
+
+    function reportProjectIsRugged(string memory id) public {
+        require(accesslist[msg.sender], "You are not authorized to perform this transaction.");
+        Project storage project = projects[id];
+
+        if(project.isRugged == false) {
+            project.isRugged = true;
+        }
+
+        project.reputation_score -= 1;
+
+        reputation_score -= 1;
+    }
+
+    function changeOwner(address _owner) public onlyOwner {
+        require(verified, "Not verified.");
+        owner = _owner;
+    }
+
+    function addAccountToAccessList(address account) public onlySuperOwner {
+        access_list.push(account);
+
+        accesslist[account] = true;
+    }
+
+    function removeAccountFromAccessList(address account) public onlySuperOwner {
+        accesslist[account] = false;
+    }
+
+    function removeVerification() public onlySuperOwner {
+        verified = false;
     }
 }
